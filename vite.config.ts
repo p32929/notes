b2fe4ad90196e9509a758ea3453efd078f7ceb7e
Fayaz <fayaz@herogram.app>
2024-08-08 19:05:43 +0400

    rem unw

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       tsDecorators: true
-    }),
-    checker({
-      typescript: false
     })
   ],
   server: {
